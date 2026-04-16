ff2e1447d89d449f27b6f6bf06ffb98fffc543c0
lcs.servant04 <lcs.servant04@gmail.com>
2026-04-16 16:37:03 +0200

    suppression_ancien_code

--- diagnostic.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-for sep in [";", ",", "\t", "|"]:
-    df = pd.read_csv("data-es-equipement.csv", sep=sep, nrows=5)
-    print(f"sep='{sep}' → {df.shape[1]} colonnes : {list(df.columns)[:5]}")
